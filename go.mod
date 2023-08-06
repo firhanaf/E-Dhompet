b7f7497bdd7592754dbadbefa909110a9df2953c
Firhan Ahmad F <firhanahmadf@gmail.com>
2023-08-07 00:40:30 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module be18/acc-service
+
+go 1.20
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type User struct {
+	Id       string
+	Username string
+	Name     string
+	Email    string
+	Password string
+	Address  string
+	Phone    string
+}
+
+func main() {
+
+	var db *sql.DB
+	var err error
+
+	//Setting db connection
+	var connectionString = os.Getenv("CONNECTION_DB")
+	// fmt.Println("connectionstring:", connectionString)
+	db, err = sql.Open("mysql", connectionString)
+	if err != nil { // jika terjadi error
+		log.Fatal("error open connection to db ", err.Error())
+	}
+	// db.SetConnMaxLifetime(time.Minute * 3)
+	// db.SetMaxOpenConns(10)
+	// db.SetMaxIdleConns(10)
+
+	errPing := db.Ping()
+	if errPing != nil {
+		log.Fatal("error connect to db ", errPing.Error())
+	} else {
+		fmt.Println("CONNECTED")
+	}
+
+	//Main Menu
+	fmt.Println("Selamat Datang di E-Dhompet")
+	fmt.Println("Pilih Menu: \n 1. Register \n 2. Login \n 3. View Profile \n 4. Update Profile \n 5. Delete Account \n 6. Top Up \n 7. Transfer \n 8. Top Up History \n 9. Transfer History \n 10. View Profile by Phone Number \n 0. Log Out")
+	var menu int
+	fmt.Println("input pilihan anda:")
+	fmt.Scanln(&menu)
+
+	switch menu {
+	case 1: // fitur Register
+	case 2: // fitur Login
+	case 3: // view profile yang telah login
+	case 4: // update profil
+	case 5: // hapus akun
+	case 6: // fitur topup saldo
+	case 7: // fitur transfer dana
+	case 8: // fitur melihat history topup
+	case 9: // fitur melihat history transfer
+	case 10: // fitur melihat profil user lain dengan menggunakan phone number
+	case 0: // fitur logout
+
+	} // EOF menu
+
+}
