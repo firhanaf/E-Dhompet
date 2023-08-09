5e96a1e599e411557247141cc2571fc366607456
Firhan Ahmad F <firhanahmadf@gmail.com>
2023-08-09 14:13:51 +0700

    update fitur hapus acc dan view profil by number

---
+++ controllers/topuphistory.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"be18/acc-service/entities"
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func HistoryTopup(db *sql.DB, loginUser entities.User) []entities.User {
+	var topuphistory []entities.User
+	users, loggedIn := Login(db, loginUser.Phone, loginUser.Password)
+	for _, v := range users {
+		if loggedIn {
+			history, errHistory := db.Query("select id, user_id, amount, status, transaction_time from topup where id = ?", v.Id)
+			if errHistory != nil {
+				log.Fatal("error to read topup history", errHistory.Error())
+			}
+			for history.Next() {
+				var DataHistory entities.User
+				errScan := history.Scan(&DataHistory.Topup_Id, &DataHistory.Id, &DataHistory.Amount, &DataHistory.Status, &DataHistory.Transaction_time_topup)
+				if errScan != nil {
+					log.Fatal("error to scan data", errScan.Error())
+				}
+				topuphistory = append(topuphistory, DataHistory)
+			}
+			fmt.Println("isi :", topuphistory)
+		}
+
+	}
+	return topuphistory
+}
