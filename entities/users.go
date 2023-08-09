8a4b6d4844899bd61ad53ec6134601c3d839fb18
gufranlazuardi <gufranlazuardi@gmail.com>
2023-08-09 14:50:44 +0700

    update transfer

--- entities/users.go
+++ entities/users.go
@@ -13,13 +13,7 @@ type User struct {
 	Status                 string
 	Topup_Id               int64
 	Transaction_time_topup string
-}
-
-type Transfer strcut {
-	Id					string
-	User_Id 			string
-	Receiver_userId 	string
-	Amount 				int
-	Status 				string
-	Transaction_time 	string
+	User_Id                string
+	Receiver_userId        string
+	Transaction_time       string
 }
